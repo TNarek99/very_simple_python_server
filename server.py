cb100dbab9d0b471827181540218a5e6953c7cbd
tnarek99 <narektumanyan99@gmail.com>
2017-06-20 07:46:26 -0700

    switched to bottle

--- server.py
+++ server.py
@@ -1,23 +1,7 @@
-# from socket import *
-#
-# def createServer():
-#     serversocket = socket(AF_INET, SOCK_STREAM)
-#     serversocket.bind(('localhost',9000))
-#     serversocket.listen(5)
-#     while(1):
-#         (clientsocket, address) = serversocket.accept()
-#         clientsocket.send("It works")
-#
-# createServer()
+from bottle import route, run
 
-import SimpleHTTPServer
-import SocketServer
+@route('/')
+def index():
+        return "Hello World!"
 
-PORT = 3000
-
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-
-print "server listening on port : ", PORT
-httpd.serve_forever()
+run(host='localhost', port=3000)
