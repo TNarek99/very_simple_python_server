d4bef5063756b1a7b5074c5544e1f2625c90af93
tnarek99 <narektumanyan99@gmail.com>
2017-06-20 06:35:06 -0700

    right way

--- server.py
+++ server.py
@@ -1,11 +1,23 @@
-from socket import *
+# from socket import *
+#
+# def createServer():
+#     serversocket = socket(AF_INET, SOCK_STREAM)
+#     serversocket.bind(('localhost',9000))
+#     serversocket.listen(5)
+#     while(1):
+#         (clientsocket, address) = serversocket.accept()
+#         clientsocket.send("It works")
+#
+# createServer()
 
-def createServer():
-    serversocket = socket(AF_INET, SOCK_STREAM)
-    serversocket.bind(('localhost',9000))
-    serversocket.listen(5)
-    while(1):
-        (clientsocket, address) = serversocket.accept()
-        clientsocket.send("It works")
+import SimpleHTTPServer
+import SocketServer
 
-createServer()
+PORT = 3000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "server listening on port : ", PORT
+httpd.serve_forever()
